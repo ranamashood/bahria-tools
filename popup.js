8d2b4bfd38af264bd2471a74b68610e568e59771
Mashood ur Rehman Rana <ranamashood@proton.me>
2024-07-24 21:08:20 +0500

    style: added popup styling for basic layout
    - buttons layout
    - buttons styling
    - results styling
      - it shows a full popup for result

--- popup.js
+++ popup.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cgpaElem = document.querySelector("#cgpa");
-  const btn = document.querySelector("button");
+  const resultElem = document.querySelector(".result");
+  const getCgpaBtn = document.querySelector("#get-cgpa");
 
-  chrome.storage.sync.get(["cgpa"], (result) => {
-    if (result.cgpa) {
-      cgpaElem.innerHTML = result.cgpa;
+  chrome.storage.sync.get(["cgpa"], (storage) => {
+    if (storage.cgpa) {
+      resultElem.innerHTML = value;
     }
   });
 
-  btn.addEventListener("click", () => {
+  getCgpaBtn.addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { action: "getCgpa" }, (response) => {
         console.log(response);
 
         chrome.storage.sync.set({ cgpa: response.cgpa }, () => {
-          cgpaElem.innerHTML = response.cgpa;
+          resultElem.innerHTML = response.cgpa;
+          resultElem.classList.add("result--active");
         });
-        cgpaElem.innerHTML = response.cgpa;
       });
     });
   });
